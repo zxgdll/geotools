999c04dc23849b1672a9312c5f4ee212e51a4bc0
Rob <rob@dijital.ca>
2016-02-25 08:26:05 -0800

    Fixed.

--- src/utils/findmin.cpp
+++ src/utils/findmin.cpp
@@ -9,7 +9,6 @@
 int main(int argc, char **argv) {
 	
 	std::string infile = argv[1];
-	double resolution = atoi(argv[2]);
 
 	try {
 		Raster<float> inrast(infile);
@@ -18,43 +17,34 @@ int main(int argc, char **argv) {
 
 		std::cerr << "Raster: " << inrast.cols() << ", " << inrast.rows() << std::endl;
 
-		int block = (int) (resolution / inrast.resolution());
-		std::cerr << "Block size: " << block << std::endl;
-
-		Grid<double> grid(inrast.cols() / block + 1, inrast.rows() / block + 1);
+		Grid<double> grid(inrast.cols(), inrast.rows());
 		grid.fill(-9999);
 
-		for(int r = 0; r < inrast.rows(); r += block) {
+		double nodata = inrast.nodata();
+		for(int r = 0; r < inrast.rows(); ++r) {
 			//std::cerr << "Row " << r << " of " << inrast.rows() << std::endl;
-			for(int c = 0; c < inrast.cols(); c += block) {
+			for(int c = 0; c < inrast.cols(); ++c) {
 				//std::cerr << c << ", " << r << std::endl;
-				if(!inrast.isValid(c, r)) continue;
-				double z = inrast.get(c, r);
-				int lc, lr;
-				bool cont = true;
-				do {
-					cont = false;
-					for(int rr = r - 1; rr < r + 2; ++rr) {
-						for(int cc = c - 1; cc < c + 2; ++cc) {
-							if(cc < 0 || rr < 0 || cc == c || rr == r || rr >= inrast.rows() || cc >= inrast.cols() || !inrast.isValid(cc, rr)) continue;
-							double z0 = inrast.get(cc, rr);
-							if(z0 < z) {
-								z = z0;
-								lc = cc;
-								lr = rr;
-								cont = true;
-								//std::cerr << " -> " << lc << ", " << lr << ", " << z << std::endl;
-							}
+				double z = inrast.getOrNodata(c, r);
+				if(z == nodata) continue;
+				bool min = true;
+				for(int rr = r - 1; rr < r + 2 && !min; ++rr) {
+					for(int cc = c - 1; cc < c + 2; ++cc) {
+						double z0 = inrast.getOrNodata(cc, rr);
+						if(z0 != nodata && z0 < z) {
+							min = false;
+							break;
 						}
 					}
-				} while(cont);
+				}
 
-				grid(lc / block, lr / block, z);
+				if(min)
+					grid(c, r, z);
 
 			}
 		}
 
-		std::cout << std::setprecision(9);
+		std::cout << std::setprecision(9) << "x,y,z" << std::endl;
 		for(int r = 0; r < grid.rows(); ++r) {
 			for(int c = 0; c < grid.cols(); ++c) {
 				if(grid(c, r) > -9999)
