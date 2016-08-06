2585dc0eb13056236ca986d30348227924457244
rob <rob@dijital.ca>
2016-08-06 13:18:31 -0700

    Travis; sh script for cmake upgrade.

--- travis_build_upgrade.sh
+++ travis_build_upgrade.sh
@@ -5,9 +5,9 @@ add-apt-repository -y ppa:george-edison55/cmake-3.x
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt-get update -qq
 
-echo "Package: *\nPin: release o=LP-PPA-george-edison55-cmake-3.x\nPin-Priority: 1000" > /etc/apt/preferences.d/cmake_prefs
+#echo "Package: *\nPin: release o=LP-PPA-george-edison55-cmake-3.x\nPin-Priority: 1000" > /etc/apt/preferences.d/cmake_prefs
 
-cat /etc/apt/preferences.d/cmake_prefs
+#cat /etc/apt/preferences.d/cmake_prefs
 sudo apt-cache policy cmake
 
 apt-get install -y --reinstall cmake
