6713e083e22df3859370f0c20e57f1752bd3b005
Rob <rob@dijital.ca>
2016-09-20 09:42:25 -0700

    Add method to use postgis points.

--- scripts/grid_fit.py
+++ scripts/grid_fit.py
@@ -6,6 +6,7 @@
 
 import sys
 import os
+import itertools
 import numpy as np
 from osgeo import gdal
 from scipy.optimize import curve_fit
@@ -52,6 +53,32 @@ def create_grid(popt, outfile, trans, w, h):
 			x = c * trans[1] + trans[0]
 			y = r * trans[5] + trans[3]
 			z = x * popt[0] + y * popt[1] + popt[2]
+			#print x, y, z, c, r
+			band.itemset((r, c), z)
+	ds.GetRasterBand(1).WriteArray(band)
+	ds = None
+
+def create_grid2(xdata, ydata, zdata, outfile, trans, w, h):
+	'''
+	Create a raster with the given transform and dimensions,
+	whose elevations are determined by the plane parameters
+	contained in the first argument.
+	'''
+	cols = int(w / trans[1]) + 1
+	rows = int(h / -trans[5]) + 1
+	print cols, rows
+	drv = gdal.GetDriverByName('GTiff')
+	ds = drv.Create(outfile, cols, rows, 1, gdal.GDT_Float32)
+	ds.SetGeoTransform(trans)
+
+	band = np.array([0.] * cols * rows, dtype='float32')
+	band.shape = (rows, cols)
+
+	for r in range(rows-1):
+		for c in range(cols-1):
+			x = xdata[rows - 2 - r,c]# * cols + c] #c * trans[1] + trans[0]
+			y = ydata[rows - 2 - r,c]# * cols + c] #r * trans[5] + trans[3]
+			z = zdata[rows - 2 - r,c]# * cols + c] #x * popt[0] + y * popt[1] + popt[2]
 			#print x, y, z, c, r
 			band.itemset((r, c), z)
 	ds.GetRasterBand(1).WriteArray(band)
@@ -107,10 +134,13 @@ def grid_fit(file1, file2, mask, outfile, samples=100):
 	xydata = np.array(xydata)
 	zdata = np.array(zdata)
 
-	def _f(pt, a, b, c):
+	def _f0(pt, a, b, c):
 		return pt[:,0] * a + pt[:,1] * b + c
 
-	popt, pcov = curve_fit(_f, xydata, zdata)
+	def _f2(pt, a, b, c, d, e, f, g):
+		return a * pt[:,0] ** 2 + b * pt[:,1] ** 2 + c * p[:,0] * p[:,1] + d * p[:,0] + e * p[:,1] + f + g
+
+	popt, pcov = curve_fit(_f2, xydata, zdata)
 
 	# Use the max bounds for building the gradient.
 	minx = min(trans1[0], trans2[0])
@@ -123,13 +153,88 @@ def grid_fit(file1, file2, mask, outfile, samples=100):
 	print popt
 	print pcov
 
-def load_pg_points(dbname, user, query):
+def poly_matrix(x, y, order=2):
+    """ generate Matrix use with lstsq """
+    import itertools
+    ncols = (order + 1)**2
+    G = np.zeros((x.size, ncols))
+    ij = itertools.product(range(order+1), range(order+1))
+    for k, (i, j) in enumerate(ij):
+        G[:, k] = x**i * y**j
+    return G
+
+def point_fit(file, outfile, samples):
+	'''
+	Find the best-fit parameters for fitting a plane to the point-set
+	Create an adjustment raster called outfile.
+	File is used to create the bounds and properties of the output.
+	'''
+	band1, trans1, ds1, nd1, w1, h1 = grid_open(file)
+
+	# Use the minimum bounds for collecting samples.
+	minx = trans1[0]
+	maxx = trans1[0] + ds1.RasterXSize * trans1[1]
+	maxy = trans1[3]
+	miny = trans1[3] + ds1.RasterYSize * trans1[5]
+
+	xydata = []
+	xdata = []
+	ydata = []
+	zdata = []
+	points = None
+
+	for x, y, z in samples:
+		xydata.append((x, y))
+		xdata.append(x)
+		ydata.append(y)
+		zdata.append(z)
+
+	xydata = np.array(xydata)
+	xdata = np.array(xdata)
+	ydata = np.array(ydata)
+	zdata = np.array(zdata)
+
+	def _f(pt, a, b, c):
+		return pt[:,0] * a + pt[:,1] * b + c
+
+	def _f2(pt, a, b, c, d, e, f, g):
+		return a * pt[:,0] ** 2 + b * pt[:,1] ** 2 + c * pt[:,0] * pt[:,1] + d * pt[:,0] + e * pt[:,1] + f + g
+	
+	def polyfit2d(x, y, z, order=3):
+	    ncols = (order + 1)**2
+	    G = np.zeros((x.size, ncols))
+	    ij = itertools.product(range(order+1), range(order+1))
+	    for k, (i,j) in enumerate(ij):
+	        G[:,k] = x**i * y**j
+	    m, _, _, _ = np.linalg.lstsq(G, z)
+	    return m
+
+	def polyval2d(x, y, m):
+	    order = int(np.sqrt(len(m))) - 1
+	    ij = itertools.product(range(order+1), range(order+1))
+	    z = np.zeros_like(x)
+	    for a, (i,j) in zip(m, ij):
+	        z += a * x**i * y**j
+	    return z
+
+	m = polyfit2d(xdata, ydata, zdata)
+
+	# Evaluate it on a grid...
+	nx, ny = ds1.RasterXSize, ds1.RasterYSize
+	xx, yy = np.meshgrid(np.linspace(xdata.min(), xdata.max(), nx), np.linspace(ydata.min(), ydata.max(), ny))
+	zz = polyval2d(xx, yy, m)
+
+	create_grid2(xx, yy, zz, outfile, [minx, trans1[1], 0., maxy, 0., trans1[5]], maxx-minx, maxy-miny)
+
+
+def load_pg_points(dbname, user, password, query):
 	import psycopg2
 	conn = psycopg2.connect("dbname=%s user=%s" % (dbname, user))
 	cur = conn.cursor()
 	points = []
-	for x, y in cur.execute(query):
-		points.append((x, y))
+	cur.execute(query)
+	for x, y, z in cur:
+		points.append((x, y, z))
 	cur.close()
 	conn.close()
 	return points
@@ -137,17 +242,43 @@ def load_pg_points(dbname, user, query):
 if __name__ == '__main__':
 
 	#try:
-	file1 = sys.argv[1]
-	file2 = sys.argv[2]
-	mask = sys.argv[3]
-	outfile = sys.argv[4]
-	samples = sys.argv[5]
-	if samples.startswith('pg'):
-		dbname, user, query = sys.argv[6:9]
-		samples = load_pg_points(dbname, user, query)
+	import getopt
+
+	opts, args = getopt.getopt(sys.argv[1:], 'f:g:m:o:p:q:u:s:d:')
+	dbname = None
+	dbuser = None
+	dbpass = None
+	query = None
+	file1 = None
+	file2 = None
+	outfile = None
+	samples = None
+	mask = None
+	for opt, val in opts:
+		if opt == '-f':
+			file1 = val
+		elif opt == '-g':
+			file2 = val
+		elif opt == '-m':
+			mask = val
+		elif opt == '-u':
+			dbuser = val
+		elif opt == '-d':
+			dbname = val
+		elif opt == '-q':
+			query = val
+		elif opt == '-o':
+			outfile = val
+		elif opt == '-s':
+			samples = int(samples)
+
+	if dbname and query and outfile and file1:
+		samples = load_pg_points(dbname, dbuser, dbpass, query)
+		point_fit(file1, outfile, samples)
+	elif file1 and file2 and outfile and mask and samples:
+		grid_fit(file1, file2, mask, outfile, samples)
 	else:
-		samples = int(samples)
-	grid_fit(file1, file2, mask, outfile, samples)
+		raise Exception('Fail.')
 	#except Exception, e:
 	#	print e
 	#	print 'Usage: grid_fit.py <file1>'
